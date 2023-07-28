1b1b6707b3fff5e1f5dc21076e43434c3d3ec105
Graf-Crab <graf.crab@gmail.com>
2023-07-28 16:24:08 +0300

    rename package "Model" to "model"

--- src/main/java/web/controller/CarController.java
+++ src/main/java/web/controller/CarController.java
@@ -5,12 +5,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import web.Model.Car;
-import web.service.CarService;
 import web.service.CarServiceImpl;
-
-import java.util.ArrayList;
-import java.util.List;
 
 @Controller
 public class CarController {
--- src/main/java/web/service/CarServiceImpl.java
+++ src/main/java/web/service/CarServiceImpl.java
@@ -1,7 +1,7 @@
 package web.service;
 
 
-import web.Model.Car;
+import web.model.Car;
 
 import java.util.ArrayList;
 import java.util.List;
